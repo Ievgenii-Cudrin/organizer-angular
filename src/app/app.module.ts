7ce5f6a382a9811423d7cfd7ecbcb5b6aa7ac81f
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2020-03-18 20:12:12 +0200

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalenderComponent } from './calender/calender.component';
+import { SelectorComponent } from './selector/selector.component';
+import { OrganizerComponent } from './organizer/organizer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalenderComponent,
+    SelectorComponent,
+    OrganizerComponent
   ],
   imports: [
     BrowserModule
